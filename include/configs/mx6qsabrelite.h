29d9c8d7efa7daba41be47f9ee2b4514bc723b1b
Otavio Salvador <otavio@ossystems.com.br>
2012-09-18 15:03:26 -0300

    mx6qsabrelite: Change default environment to cope with OE changes
    OpenEmbedded has change partitioning layout of generated image so it
    does not raise warnings during the boot regarding unkown partition
    being used for U-Boot.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6qsabrelite.h
+++ include/configs/mx6qsabrelite.h
@@ -138,12 +138,10 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
        "script=boot.scr\0" \
        "uimage=uImage\0" \
-	"console=ttymxc1\0" \
-	"fdt_high=0xffffffff\0"	  \
-	"initrd_high=0xffffffff\0" \
+       "console=ttymxc1\0" \
        "mmcdev=0\0" \
-       "mmcpart=2\0" \
-       "mmcroot=/dev/mmcblk0p3 rootwait rw\0" \
+       "mmcpart=1\0" \
+       "mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
        "mmcargs=setenv bootargs console=${console},${baudrate} " \
 	       "root=${mmcroot}\0" \
        "loadbootscript=" \

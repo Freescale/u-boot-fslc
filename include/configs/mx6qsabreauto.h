af1b14baf4e21f9ec1d9a349cd5cf485ec251f40
Otavio Salvador <otavio@ossystems.com.br>
2012-10-02 14:02:44 -0300

    mx6qsabreauto: Fix boot partition for OE layout
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6qsabreauto.h
+++ include/configs/mx6qsabreauto.h
@@ -76,7 +76,7 @@
 	"initrd_high=0xffffffff\0" \
 	"mmcdev=0\0" \
 	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
+	"mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} " \
 		"root=${mmcroot}\0" \
 	"loadbootscript=" \

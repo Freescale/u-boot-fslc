d8c2380eca7e4fd6cfa8a079fb5b65424be32b8a
Otavio Salvador <otavio@ossystems.com.br>
2014-03-03 23:21:15 -0300

    mx6sabresd: Use LDO dtb file until LDO bypass support is added
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6sabresd.h
+++ include/configs/mx6sabresd.h
@@ -23,9 +23,9 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
 #define CONFIG_MMCROOT			"/dev/mmcblk1p2"
 #if defined(CONFIG_MX6Q)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6q-sabresd.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6q-sabresd-ldo.dtb"
 #elif defined(CONFIG_MX6DL)
-#define CONFIG_DEFAULT_FDT_FILE	"imx6dl-sabresd.dtb"
+#define CONFIG_DEFAULT_FDT_FILE	"imx6dl-sabresd-ldo.dtb"
 #endif
 #define PHYS_SDRAM_SIZE		(1u * 1024 * 1024 * 1024)
 

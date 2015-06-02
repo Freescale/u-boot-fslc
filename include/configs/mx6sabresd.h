0ab586a7dd0fba30132bdf4a278107fd47e72a50
Nikolay Dimitrov <picmaster@mail.bg>
2015-06-02 18:59:34 +0300

    mx6sabresd: Enable video interfaces in bootargs
    Generate kernel video bootargs for sabresd, based on a list of needed video
    interfaces ("video_interfaces" U-Boot env-var). The order of initialization
    and video settings can be fully customized.
    
    Signed-off-by: Nikolay Dimitrov <picmaster@mail.bg>

--- include/configs/mx6sabresd.h
+++ include/configs/mx6sabresd.h
@@ -22,6 +22,9 @@
 
 #define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
 
+#define VIDEO_ARGS        "${video_args}"
+#define VIDEO_ARGS_SCRIPT "run video_args_script; "
+
 #include "mx6sabre_common.h"
 
 #define CONFIG_SYS_FSL_USDHC_NUM	3

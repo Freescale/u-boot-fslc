2da6710da40e32dd3d9b4ae5e22f38ec1c466eca
Otavio Salvador <otavio@ossystems.com.br>
2015-07-14 19:03:00 -0300

    wandboard: Drop revision specific DeviceTree support
    The 3.10.53 and 3.14.28 Wandboard kernels handles the revision in a
    single DeviceTree file so the boot script needs to respect that.
    
    This should be reworked in the Wandboard kernel to make it to use the
    same schema adppted by the Linux mainline and allow an easy change
    between them, however, this need be coordinated before we can do this
    in the FSL Community fork as well.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/wandboard.h
+++ include/configs/wandboard.h
@@ -146,14 +146,10 @@
 		"fi; " \
 		"setenv bootargs ${bootargs} ${fbmem}\0" \
 	"findfdt="\
-		"if test $board_name = C1 && test $board_rev = MX6Q ; then " \
+		"if test $board_rev = MX6Q ; then " \
 			"setenv fdtfile imx6q-wandboard.dtb; fi; " \
-		"if test $board_name = C1 && test $board_rev = MX6DL ; then " \
+		"if test $board_rev = MX6DL ; then " \
 			"setenv fdtfile imx6dl-wandboard.dtb; fi; " \
-		"if test $board_name = B1 && test $board_rev = MX6Q ; then " \
-			"setenv fdtfile imx6q-wandboard-revb1.dtb; fi; " \
-		"if test $board_name = B1 && test $board_rev = MX6DL ; then " \
-			"setenv fdtfile imx6dl-wandboard-revb1.dtb; fi; " \
 		"if test $fdtfile = undefined; then " \
 			"echo WARNING: Could not determine dtb to use; fi; \0" \
 	"kernel_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \

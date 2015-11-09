2fdfdeada2c490f9a61917344046825897f15622
Fabio Estevam <fabio.estevam@freescale.com>
2015-11-09 14:04:31 -0200

    mx6sabre_common: Dynamically change the video output
    Use the 'hdmidetect' command in U-boot to load the appropriate
    video arguments. If HDMI is connected during boot then make HDMI
    as the primary framebuffer, otherwise let LVDS be the primary framebuffer.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -67,6 +67,13 @@
 #define VIDEO_ARGS_SCRIPT ""
 #endif
 
+#define CONFIG_PREBOOT \
+	"if hdmidet; then " \
+		"setenv video_interfaces hdmi lvds; " \
+	"else " \
+		"setenv video_interfaces lvds hdmi; " \
+	"fi;"
+
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"image=zImage\0" \
@@ -105,7 +112,6 @@
 	"video_args_lcd=setenv video_args $video_args " \
 		"video=mxcfb${fb}:dev=lcd,CLAA-WVGA,if=RGB666\0" \
 	"fb=0\0" \
-	"video_interfaces=hdmi lvds lcd\0" \
 	"video_args_script=" \
 		"for v in ${video_interfaces}; do " \
 			"run video_args_${v}; " \

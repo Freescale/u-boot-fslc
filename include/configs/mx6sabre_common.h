cfc4a96fb9b60c5986a39eff115d2e90df2ce543
Fabio Estevam <fabio.estevam@nxp.com>
2016-03-21 11:00:53 -0300

    mx6sabre_common: Fix U-Boot corruption after 'saveenv'
    Booting mx6qp sabreauto board and then doing:
    
    => saveenv
    => reset
    
    , causes a system hang.
    
    This happens because the size of the U-Boot binary is larger than
    CONFIG_ENV_OFFSET.
    
    Fix this problem by increasing CONFIG_ENV_OFFSET, so that the U-boot binary
    and the environment variables region do not overlap.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>

--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -232,7 +232,7 @@
 #define CONFIG_ENV_IS_IN_MMC
 
 #if defined(CONFIG_ENV_IS_IN_MMC)
-#define CONFIG_ENV_OFFSET		(8 * 64 * 1024)
+#define CONFIG_ENV_OFFSET		(768 * 1024)
 #endif
 
 /* Framebuffer */

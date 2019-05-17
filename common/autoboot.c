e394b38dffeddfbd5662de06ddc13832ad0e666c
Ye Li <ye.li@nxp.com>
2019-05-17 01:52:27 -0700

    MLK-21854-2 imx: uuu: Update autoboot to run uuu or fastboot
    When booting from USB device, change u-boot autoboot to default
    run UUU bootcmd or enter fastboot directly for android.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit eba8710106348a283940b851080b049bc7fbe466)
    (cherry picked from commit decfa984826db8ae39e93f2968b8a04d009eef0d)

--- common/autoboot.c
+++ common/autoboot.c
@@ -23,7 +23,12 @@
 #include <linux/delay.h>
 #include <u-boot/sha256.h>
 #include <bootcount.h>
+
 #include <crypt.h>
+
+#ifdef is_boot_from_usb
+#include <env.h>
+#endif
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -449,6 +454,21 @@ const char *bootdelay_process(void)
 		bootdelay = fdtdec_get_config_int(gd->fdt_blob, "bootdelay",
 						  bootdelay);
 
+#if defined(is_boot_from_usb)
+	if (is_boot_from_usb() && env_get("bootcmd_mfg")) {
+		disconnect_from_pc();
+		printf("Boot from USB for mfgtools\n");
+		bootdelay = 0;
+		env_set_default("Use default environment for \
+				 mfgtools\n", 0);
+	} else if (is_boot_from_usb()) {
+		printf("Boot from USB for uuu\n");
+		env_set("bootcmd", "fastboot 0");
+	} else {
+		printf("Normal Boot\n");
+	}
+#endif
+
 	debug("### main_loop entered: bootdelay=%d\n\n", bootdelay);
 
 	if (IS_ENABLED(CONFIG_AUTOBOOT_MENU_SHOW))
@@ -464,6 +484,13 @@ const char *bootdelay_process(void)
 		s = env_get("altbootcmd");
 	else
 		s = env_get("bootcmd");
+
+#if defined(is_boot_from_usb)
+	if (is_boot_from_usb() && env_get("bootcmd_mfg")) {
+		s = env_get("bootcmd_mfg");
+		printf("Run bootcmd_mfg: %s\n", s);
+	}
+#endif
 
 	if (IS_ENABLED(CONFIG_OF_CONTROL))
 		process_fdt_options(gd->fdt_blob);
